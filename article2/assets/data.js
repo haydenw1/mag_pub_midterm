9ac8557072d3cc29c5f92c1d0d3f1d4e7929cc72
Hayden Wagner <hbw6955@rit.edu>
2015-10-14 02:41:18 -0400

    change structure to include second article and slider files

---
+++ article2/assets/data.js
@@ -0,0 +1,74 @@
+var data = {
+  ryan: {
+    name: "Ryan Walsh",
+    rf: "Has a 9 year old Welsh Springer Spaniel named Bailey",
+    dj: "Deep-sea donut fisher",
+    one: [
+      "Back in highschool I experimented with an independent study in printmaking. The entire process was very interesting and I was beginning to understand ink layering. This was my first exposure to the world of print. When I toured RIT I was initially interested in the co-op program. I was also interested in other majors such as Graphic Design and Industrial Design.",
+      "This major is unique because it allows the students to branch off into different paths whether it be website coding, design, or print production while still handling different types of media."
+    ],
+    two: [
+      "I would describe Media Arts and Technology as the transformation of content between web and print across multiple media. It’s important to understand the printing process when you’re designing for print. Certain constraints restrict how the designs are created and viewed, especially in print publications such as magazines and books. We receive the toolset and training to help businesses publish their content to different formats including the web.",
+      "Advertising and Public Relations has a strong connection with our major. Most students are taking Advertising and Public Relations as either a minor or immersion. It helps build our communication skills and connects with the idea of publishing to different platforms so that we can reach viewers."
+    ],
+    three: [
+      "Throughout the past four years that I’ve been at RIT, I’ve received plenty of support from the faculty and other students. The classes have been informative and relevant and I’ve been building up work experience at the SMS Labs and at Reporter Magazine as the Production Manager. Classes and work keep me occupied throughout the week, however I also enjoy extracurricular activities such as Concert Band, racquetball, and ultimate frisbee."
+    ]
+  },
+  tatiana: {
+    name: "Tatiana Morales",
+    rf: "Owns the entire Underworld collection, but has never watched them.",
+    dj: "Photoshop Expert, Print Project Manager",
+    one: [
+      "I first came to RIT as a Game Design and Development major.  After a year, I decided to transfer to something that allowed me to explore my creative talents.  The MAAT major was the perfect blend of technology and creativity that best suited my interests."
+    ],
+    two: [
+      "I often think of MAAT as a major that produces jacks of all trades.  Print, digital, design and programming are all covered in the core curriculum where professional electives allow the students to specialize or tailor their skill sets."
+    ],
+    three: [
+      "I remember how unsure I was when I made the decision to change majors.  I was unsure of my skills, my abilities and my confidence in what I could in the work force.  It wasn’t until I started taking classes as a MAAT major and finding myself excel in them did I start to feel confident in the skills I was creating.  My experience at RIT makes me excited for my future.  I want to see how else I can expand upon my abilities and bring what I’ve learned in the classroom out into the world."
+    ]
+  },
+  marcus: {
+    name: "Markus Hines",
+    rf: "I run track.",
+    dj: "To be a traveling artist or creating movie posters in Photoshop.",
+    one: [
+      "Originally this wasn’t my first choice. My first choice was New Media Design, but I didn’t qualify for it so they put me in this major instead.  Originally I didn’t like it, specifically because of foundations, but i started liking it more as I took Imaging and Typography. "
+    ],
+    two: [
+      "I would define it as a more logical design."
+    ],
+    three: [
+      "I would describe it as a very interesting experience.  This major we’ve done a wide range of different things from Foundations, which is understanding how it works to Typography which is like the text of a file to Imaging which is photos of the file.  So it's been pretty interesting."
+    ]
+  },
+  tommy: {
+    name: "Tommy Hines",
+    rf: "In high school I won a baseball state championship.",
+    dj: "I honestly don't know yet. I enjoy both the creative and production sides of media and I would like to do anything in those two Umbrellas. I think it would be really cool to work at a company like ESPN though.",
+    one: [
+      "I chose this major because, in my eyes, it is the perfect  intersection between creative and technical production of media. I like having the freedom to choose where I take the major. I think that being a Media Arts and Technology major allows me to really define what I want my college experience to be like."
+    ],
+    two: [
+      "Media Arts and Technology, to me, is where creative media design meets media production. Within the major, any student can make it as creative or technical or production oriented as they wish. It is slightly less structured, in terms of exact coursework, than other majors I have seen at RIT."
+    ],
+    three: [
+      "I have had a very good experience at RIT. I really enjoy my classes. I think that Media Art's and Technology has given me a good amount of freedom in choosing classes and class times which will work best for me. I joined a fraternity my first semester on campus and that was one of the best decisions I have made in my time here. I love the facilities RIT has at the disposal of its students, especially the gym area and pool."
+    ]
+  },
+  hayden: {
+    name: "Hayden Wagner",
+    rf: "I have an orange vacuum",
+    dj: "Rope-swing builder",
+    one: [
+      "I chose this major because I was interested in combining technical and design elements of media development, and this major seemed to fit this the best. I was a student in the New Media Interactive Development program my first year at RIT, but I switched to Media Arts and Technology because I wanted a major more involved with design production."
+    ],
+    two: [
+      "Media Arts and Technology is a major that combines design, print, and digital media; it allows students to experience a wide range of media and the techniques to develop them. In the later years in this major students can choose a specific aspect of the major to specialize in."
+    ],
+    three: [
+      "My time at RIT has been great, I am amazed by how great the teachers are and how committed they are to their students learning. Getting involved on campus has made my experience great at RIT."
+    ]
+  },
+};
