2133d233db9d5cc27451a5a9cbd6c06fb185ab8b
Hayden Wagner <hbw6955@rit.edu>
2015-10-15 02:19:31 -0400

    Fixed up both articles -- 95% there

--- article1/js/main.js
+++ article1/js/main.js
@@ -1,65 +1,79 @@
+//object to create block function scope
 var maat = {
-  selected: "ryan",
+  selected: "ryan", //property to keep track of the currect selected student...defaults to Ryan Walsh
 
+  /**
+   * Function that is called upon DOM page-load--attaches 'click' event listener (which also functions
+   * as touch) to the buttons that the user can pick from.
+   */
   setup: function() {
-    console.log(data);
-    console.log("coleslaw");
-
     $( '.student-button' ).on( "click", function(){
+      //if this is not the current selection
       if(this.id != maat.selected) {
-        maat.switchThings(this);
+        maat.switchThings(this); //run the switch function
       }
     });
   },
 
+  /**
+   * Function that is called when a user makes a selection of a student button. Uses the main
+   * data object ('data.js') to switch the student data and paragraphs out (dependent on the id
+   * that is attached to the <div> that is pressed--used first name of student as id).
+   * @param  {object} touched Button <div> that is touched by the user
+   */
   switchThings: function(touched) {
-    var divsToSwitch = ($(".switch"));
-    var studentData = data[touched.id];
+    var divsToSwitch = ($( ".switch" )); //selects switch divs
+    var studentData = data[touched.id]; //records id of touched button (student name)
 
-    $( ".switch" ).empty();
+    $( ".switch" ).empty(); //empties all divs that hold paragraphs to be changed.
 
+    //for every paragraph the student wrote for question one, calls 'makePara' function
     for(a = 0; a < studentData.one.length; a++) {
       maat.makePara(divsToSwitch[0], studentData.one[a]);
     }
 
+    //for every paragraph the student wrote for question two, calls 'makePara' function
     for(b = 0; b < studentData.two.length; b++) {
       maat.makePara(divsToSwitch[1], studentData.two[b]);
     }
 
+    //for every paragraph the student wrote for question three, calls 'makePara' function
     for(c = 0; c < studentData.three.length; c++) {
       maat.makePara(divsToSwitch[2], studentData.three[c]);
     }
 
+    //changes student name and details
     $( ".student-name").html(studentData.name);
     $( ".year-level" ).html("<span class='in-text-title'>Year Level: </span>" + studentData.yl);
     $( ".random-fact" ).html("<span class='in-text-title'>Random Fact: </span>" + studentData.rf);
     $( ".dream-job" ).html("<span class='in-text-title'>Dream Job: </span>" + studentData.dj);
 
+    //fades out old student image..when complete > switches src to new one, and then fades back in
     $( "#image" ).stop().fadeTo( "fast", 0, function() {
       $( "#image" ).attr("src", "assets/photos/" + touched.id + ".jpg");
       $( "#image" ).stop().fadeTo( "slow", 1);
     });
 
+    touched.src = "assets/buttons/selected.jpg"; //switches newly selected button to "selected.jpg" (arrow)
+    touched.className = "student-button"; //changes class of selected button (to remove small shadow)
 
-    //.fadeOut().attr("src", "assets/photos/" + touched.id + ".jpg");
-
-    touched.src = "assets/buttons/selected.jpg";
-    touched.className = "student-button";
+    //returns previously selected button to unselected state
     $( "#"+ maat.selected )
-      .attr("src", "assets/buttons/" + maat.selected + ".png")
+      .attr("src", "assets/buttons/" + maat.selected + ".jpg")
       .attr("class", "student-button floaty");
+
+    //records the new selection
     maat.selected = touched.id;
 
   },
 
-  //clearDivs: function(div) {
-  //  console.log(div);
-  //  $( div ).empty();
-  //},
-
+  /**
+   * Appends a <p> tag in the passed div with the passed student data
+   * @param  {object} div  Object (div) where we will append <p> tag
+   * @param  {string} data Data string from main data object--in this case it is
+   *                       a paragraph of a student's answer to one of the questions
+   */
   makePara: function(div, data) {
-    console.log("make para");
-    console.log(data);
     $( div ).append( "<p>" + data + "</p>");
   }
 };
--- article2/js/main.js
+++ article2/js/main.js
@@ -1,64 +1,17 @@
+//object to create block function scope
 var maat = {
-  selected: "ryan",
 
+  /**
+   * Function that is called upon DOM page-load for basic element adjustments in this case
+   */
   setup: function() {
-    console.log(data);
-    console.log("coleslaw");
+    var sliderImgHeight = $( "#img-height" ).height(); //gets the image height of one image in the slider
 
-    $( '.student-button' ).on( "click", function(){
-      if(this.id != maat.selected) {
-        maat.switchThings(this);
-      }
-    });
+    //checks if dots are there, if so, sets position of dots based on image size
+    if ( $( ".dots" ) ) {
+      $( ".dots" ).css("top", (sliderImgHeight * 0.9) + "px");
+    }
+
+    $('audio').prop("volume", 0.5); //sets default volume of audio element to half
   },
-
-  switchThings: function(touched) {
-    var divsToSwitch = ($(".switch"));
-    var studentData = data[touched.id];
-
-    $( ".switch" ).empty();
-
-    for(a = 0; a < studentData.one.length; a++) {
-      maat.makePara(divsToSwitch[0], studentData.one[a]);
-    }
-
-    for(b = 0; b < studentData.two.length; b++) {
-      maat.makePara(divsToSwitch[1], studentData.two[b]);
-    }
-
-    for(c = 0; c < studentData.three.length; c++) {
-      maat.makePara(divsToSwitch[2], studentData.three[c]);
-    }
-
-    $( ".student-name").html(studentData.name);
-    $( ".random-fact" ).html("<span class='in-text-title'>Random Fact: </span>" + studentData.rf);
-    $( ".dream-job" ).html("<span class='in-text-title'>Dream Job: </span>" + studentData.dj);
-
-    $( "#image" ).stop().fadeTo( "fast", 0, function() {
-      $( "#image" ).attr("src", "assets/photos/" + touched.id + ".jpg");
-      $( "#image" ).stop().fadeTo( "slow", 1);
-    });
-
-
-    //.fadeOut().attr("src", "assets/photos/" + touched.id + ".jpg");
-
-    touched.src = "assets/buttons/selected.jpg";
-    touched.className = "student-button";
-    $( "#"+ maat.selected )
-      .attr("src", "assets/buttons/" + maat.selected + ".png")
-      .attr("class", "student-button floaty");
-    maat.selected = touched.id;
-
-  },
-
-  //clearDivs: function(div) {
-  //  console.log(div);
-  //  $( div ).empty();
-  //},
-
-  makePara: function(div, data) {
-    console.log("make para");
-    console.log(data);
-    $( div ).append( "<p>" + data + "</p>");
-  }
 };
